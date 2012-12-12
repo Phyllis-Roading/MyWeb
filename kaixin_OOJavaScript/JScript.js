c36485ddcb2b1ca4d41cee377ac9cda80cd51fe8
李玉刚 <442607135@qq.com>
2012-12-12 12:48:51 +0800

    Web

---
+++ kaixin_OOJavaScript/JScript.js
@@ -0,0 +1,251 @@
+﻿
+
+//导航栏
+        function Navigation_over()
+        {
+             
+             var str=document.getElementById("navigation");
+             str.style.display="block";
+        }
+       
+        function Navigation_out()
+        {
+             var str=document.getElementById("navigation");
+             str.style.display="none";
+        }
+//导航栏结束   
+//*************************************************
+//邮箱        
+        function mail_Occur()
+        {
+             var mail_str=document.getElementById("mail");
+             mail_str.style.borderTopColor="black";
+             mail_str.style.borderLeftColor="black";
+             document.getElementById("mail_check_div_Y").style.display="none";
+             document.getElementById("mail_check_div_N").style.display="none";
+             document.getElementById("mail_tip").style.display="block";
+             if(mail_str.value!="")
+             {
+                  mail_box_a_follow();
+             }
+        }
+        
+        function mail_Hiding()
+        {
+             var mail_str=document.getElementById("mail");
+             mail_str.style.borderColor="";
+             document.getElementById("mail_tip").style.display="none";
+             document.getElementById("mail_box").style.display="none";
+             if(mail_str.value.indexOf("@qq.com")>0 || mail_str.value.indexOf("@126.com")>0 
+             || mail_str.value.indexOf("@163.com")>0 || mail_str.value.indexOf("@sina.com")>0 
+             || mail_str.value.indexOf("@sina.cn")>0 || mail_str.value.indexOf("@hotmail.com")>0 
+             || mail_str.value.indexOf("@gmail.com")>0 || mail_str.value.indexOf("@sohu.cn")>0 
+             || mail_str.value.indexOf("@yahoo.cn")>0 || mail_str.value.indexOf("@139.cn")>0)
+             {
+                   document.getElementById("mail_check_div_Y").style.display="block";
+             }
+             else if(mail_str.value==""){}
+             else{document.getElementById("mail_check_div_N").style.display="block";}
+        }
+        function mail_box_a_follow()
+        {
+             document.getElementById("mail_box").style.display="block";
+             var mail_txt=document.getElementById("mail").value;
+             if( mail_txt.indexOf("@")<0 )
+             {
+                 document.getElementById("mail_qq").innerHTML=mail_txt+"@qq.com";
+                 document.getElementById("mail_126").innerHTML=mail_txt+"@126.com";
+                 document.getElementById("mail_163").innerHTML=mail_txt+"@163.com";
+                 document.getElementById("mail_sinaM").innerHTML=mail_txt+"@sina.com";
+                 document.getElementById("mail_sinaN").innerHTML=mail_txt+"@sina.cn";
+                 document.getElementById("mail_hotmail").innerHTML=mail_txt+"@hotmail.com";
+                 document.getElementById("mail_gmail").innerHTML=mail_txt+"@gmail.com";
+                 document.getElementById("mail_sohu").innerHTML=mail_txt+"@sohu.cn";
+                 document.getElementById("mail_yahoo").innerHTML=mail_txt+"@yahoo.cn";
+                 document.getElementById("mail_139").innerHTML=mail_txt+"@139.cn";                 
+             }
+        }
+        
+        function choosemail()
+        {
+            document.getElementById("mail").value="asdfdsfdsfsdf";
+        }
+        
+//邮箱结束 
+//*****************************************
+//密码      
+        function pwd_Occur()
+        {
+             document.getElementById("pwd").style.borderTopColor="black";
+             document.getElementById("pwd").style.borderLeftColor="black";
+             document.getElementById("pwd_check_div_Y").style.display="none";
+             document.getElementById("pwd_check_div_N").style.display="none";
+             document.getElementById("pwd_tip").style.display="block";
+        }
+        
+        function pwd_Hiding()
+        {
+             var pwd_str=document.getElementById("pwd");
+             pwd_str.style.borderColor="";
+             document.getElementById("pwd_tip").style.display="none";
+             if(pwd_str.value.length>=6 && pwd_str.value.length<=20)
+             {
+                 document.getElementById("pwd_check_div_Y").style.display="block";
+             }
+             else if(pwd_str.value==""){}
+             else{document.getElementById("pwd_check_div_N").style.display="block";}
+        }
+//密码结束
+//*******************************************
+//姓名
+        function user_name_Occur()
+        {
+             document.getElementById("user_name").style.borderTopColor="black";
+             document.getElementById("user_name").style.borderLeftColor="black";
+             document.getElementById("user_name_tip").style.display="block";
+             
+        }
+        
+        function user_name_Hiding()
+        {
+             document.getElementById("user_name").style.borderColor="";
+             document.getElementById("name_tip").style.display="none";
+        }
+//姓名结束
+  //*********************************   
+  //性别
+       function sex_checkbox()
+       {
+            document.getElementById("sex_check_div_Y").style.display="block";
+       }
+ // 性别结束
+ //**************************************
+ //日期
+function panduan()
+{
+    var year=document.getElementById("year").selectedIndex;
+    var month=document.getElementById("month").selectedIndex;
+    
+    switch(month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+             document.getElementById("day29").style.display="inline";
+             document.getElementById("day30").style.display="inline";
+             document.getElementById("day31").style.display="inline";
+             break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+             document.getElementById("day29").style.display="inline";
+             document.getElementById("day30").style.display="inline";
+             document.getElementById("day31").style.display="none";
+             break;
+        default:
+            if(runnian(year))
+            {
+                document.getElementById("day29").style.display="inline";
+                document.getElementById("day30").style.display="none";
+                document.getElementById("day31").style.display="none";
+            }
+            else
+            {
+                document.getElementById("day29").style.display="none";
+                document.getElementById("day30").style.display="none";
+                document.getElementById("day31").style.display="none";
+            }
+            break;
+        
+    }
+}
+function runnian(year)//判断是否是闰年
+{
+    if((year%4==0&&year%100!=0)||(year%400==0))
+        return true;
+    else
+        return false;
+}
+function date_select()
+{
+    if(document.getElementById("year").value1="请选择" && document.getElementById("month").value!="--" && document.getElementById("day").value!="--")
+    {
+          document.getElementById("birthday_check_div_Y").style.display="block";
+    }
+}
+//日期结束
+//**********************************
+ //现状
+      function state_checkbox()
+      {
+           document.getElementById("state_check_div_Y").style.display="block";
+      }
+ //现状结束
+ //页面加载
+ //城市选择
+ 
+ function city_choose()
+ {
+     document.getElementById("city_box").style.display="block";
+ }
+ 
+ function city_close()
+ {
+     document.getElementById("city_box").style.display="none";
+ }
+ //
+ //函数调用       
+window.onload=function ()
+{
+    if(navigator.appName=="Microsoft Internet Explorer")
+    {
+         document.getElementById("daohang").attachEvent("onmouseover",Navigation_over);
+         document.getElementById("daohang").attachEvent("onmouseout",Navigation_out );
+         document.getElementById("navigation").attachEvent("onmouseover",Navigation_over );
+         document.getElementById("navigation").attachEvent("onmouseout",Navigation_out );
+         document.getElementById("mail").attachEvent("onfocus",mail_Occur );
+         document.getElementById("mail").attachEvent("onblur",mail_Hiding );
+         document.getElementById("mail").attachEvent("onkeyup",mail_box_a_follow );
+         document.getElementById("pwd").attachEvent("onfocus",pwd_Occur );
+         document.getElementById("pwd").attachEvent("onblur",pwd_Hiding );
+         document.getElementById("user_name").attachEvent("onfocus",user_name_Occur );
+         document.getElementById("user_name").attachEvent("onblur",user_name_Hiding );
+          //选择邮箱类型
+          
+         document.getElementById("checkbox_M").attachEvent("onclick",sex_checkbox );
+         document.getElementById("checkbox_W").attachEvent("onclick",sex_checkbox );
+         document.getElementById("checkbox_working").attachEvent("onclick",state_checkbox );
+         document.getElementById("checkbox_studying").attachEvent("onclick",state_checkbox );
+
+    }
+    else
+    {
+         document.getElementById("daohang").addEventListener("mouseover",Navigation_over,false);
+         document.getElementById("daohang").addEventListener("mouseout",Navigation_out,false);
+         document.getElementById("navigation").addEventListener("mouseover",Navigation_over,false);
+         document.getElementById("navigation").addEventListener("mouseout",Navigation_out,false);
+         document.getElementById("mail").addEventListener("focus",mail_Occur,false);
+         document.getElementById("mail").addEventListener("blur",mail_Hiding,false);
+         document.getElementById("mail").addEventListener("keyup",mail_box_a_follow,false);
+         document.getElementById("pwd").addEventListener("focus",pwd_Occur,false);
+         document.getElementById("pwd").addEventListener("blur",pwd_Hiding,false);
+         document.getElementById("user_name").addEventListener("focus",user_name_Occur,false);
+         document.getElementById("user_name").addEventListener("blur",user_name_Hiding,false);
+         //选择邮箱类型
+         document.getElementById("mail_qq").addEventListener("click",choosemail,false);
+         document.getElementById("checkbox_M").addEventListener("click",sex_checkbox,false);
+         document.getElementById("checkbox_W").addEventListener("click",sex_checkbox,false);
+         document.getElementById("checkbox_working").addEventListener("click",state_checkbox,false);
+         document.getElementById("checkbox_studying").addEventListener("click",state_checkbox,false);
+         
+         document.getElementById("month").addEventListener("change",panduan,false);
+         document.getElementById("day").addEventListener("change",date_select,false);
+
+    }
+
+}
---
+++ kaixin_OOJavaScript/OO_JScript.js
@@ -0,0 +1,206 @@
+﻿window.addEvent =function(element,event,fn){
+  if(element.addEventListener)
+  {
+	  element.addEventListener(event,fn,false);
+	  
+  }
+  else if(element.attachEvent){
+     element.attachEvent("on"+event, function(){ fn.call(o) });
+     
+   }
+}
+function tip(title,isInfo,id)
+{
+  this.title=title;
+  this.isInfo=isInfo;
+  this.id=id;
+  this.tipY_Dom=document.getElementById(id+"_check_div_Y");
+  
+}
+tip.prototype=(function()
+{
+	//生成合适的Tip的内部函数
+    function genTip(id,title,isInfo)
+	{
+     if(isInfo=="judge")
+     {
+         if(judge(id))
+         {
+             document.getElementById(id+"_check_div_Y").innerHTML='<img src="picture/Yes.jpg" alt="" />';
+         }
+         else
+         {
+             document.getElementById(id+"_check_div_N").innerHTML='<div style="float:left"><div style="float: left">'+
+             '<img src="picture/No.jpg" alt="" /></div><img src="picture/xiaojiantouY_'+id+'.jpg" alt="" />'+
+             '</div><div class="'+id+'_tip_N"><span style="padding-left: 5px">'+title+'</span></div>';
+         }
+      
+      }
+	 else
+	 {
+	     document.getElementById(id+"_tip").innerHTML='<div style="float: left"><img src="picture/xiaojiantouW_'+id+'.jpg" alt="" /></div>'+
+	     '<div class="'+id+'_tip"><span style="padding-left: 5px">'+title+'</span></div>';
+	 }
+	}
+
+	//王华杰 不能在此处应用this
+	//alert(this.title+" in closure");
+	return {
+        showTip:function()
+		{
+			genTip(this.id,this.title,this.isInfo);
+			document.getElementById(this.id).style.borderTopColor="black";
+			document.getElementById(this.id).style.borderLeftColor="black";
+            document.getElementById(this.id+"_tip").style.display="block";
+			document.getElementById(this.id+"_check_div_Y").style.display="none";
+			document.getElementById(this.id+"_check_div_N").style.display="none";
+			if(this.id=="mail" && document.getElementById(this.id).value!="")
+             {
+                  mail_box_a_follow();
+             }
+
+		},
+		hideTip:function()
+		{
+			genTip(this.id,this.title,this.isInfo);
+			document.getElementById(this.id).style.borderTopColor="";
+			document.getElementById(this.id).style.borderLeftColor="";
+            document.getElementById(this.id+"_tip").style.display="none";
+            if(this.id=="mail"){document.getElementById(this.id+"_box").style.display="none";}else{}
+            if(judge(this.id))
+            {
+               document.getElementById(this.id+"_check_div_Y").style.display="block";
+            }
+            else if(document.getElementById(this.id).value==""){}
+			else
+            {
+               document.getElementById(this.id+"_check_div_N").style.display="block";
+            }
+		}
+	}
+})();
+
+
+	function judge(id)
+	{
+		var result;
+	   if(id=="mail")
+	   {
+	       if(document.getElementById(id).value.indexOf("@qq.com")>0 || document.getElementById(id).value.indexOf("@126.com")>0 
+             || document.getElementById(id).value.indexOf("@163.com")>0 || document.getElementById(id).value.indexOf("@sina.com")>0 
+             || document.getElementById(id).value.indexOf("@sina.cn")>0 || document.getElementById(id).value.indexOf("@hotmail.com")>0 
+             || document.getElementById(id).value.indexOf("@gmail.com")>0 || document.getElementById(id).value.indexOf("@sohu.cn")>0 
+             || document.getElementById(id).value.indexOf("@yahoo.cn")>0 || document.getElementById(id).value.indexOf("@139.cn")>0)
+             {
+                   result=true;
+             }
+             else{result=false;}
+             
+	   }
+	   else if(id=="pwd")
+	   {
+	       if(document.getElementById(id).value.length>=6 && document.getElementById(id).value.length<=20){result=true;}
+	       else{result=false;}
+            
+	   }
+	   else
+	   {
+	       for(var i=0;i<document.getElementById(id).value.length;i++) 
+           {
+               result=document.getElementById(id).value.charCodeAt(i)>=10000 ;
+            }
+           if(result && (document.getElementById(id).value.length==3||document.getElementById(id).value.length==2)){result=true;}
+	       else{ result=false;}
+        }
+	   return result;
+	}
+
+
+
+function prepareTip()
+ {
+    //邮箱操作
+	var mail_event=document.getElementById("mail");
+	window.addEvent(mail_event,'blur',blurFn);
+	window.addEvent(mail_event,'focus',focusFn);
+	window.addEvent(mail_event,'keyup',mail_box_a_follow);
+	//密码操作
+	var pwd_event=document.getElementById("pwd");
+	window.addEvent(pwd_event,'blur',blurFn);
+	window.addEvent(pwd_event,'focus',focusFn);
+	//用户名操作
+	var user_name_event=document.getElementById("user_name");
+	window.addEvent(user_name_event,'focus',focusFn);
+	window.addEvent(user_name_event,'blur',blurFn);
+	/* 暂时注销掉
+	var n=document.getElementById("name");
+	window.addEvent(n,'blur',blurFn);
+	window.addEvent(n,'focus',focusFn);
+	var p=document.getElementById("password");
+	window.addEvent(p,'blur',blurFn);
+	window.addEvent(p,'focus',focusFn);
+	*/
+ }
+ function mail_box_a_follow()
+{
+    document.getElementById("mail_box").style.display="block";
+    var mail_txt=document.getElementById("mail").value;
+    if( mail_txt.indexOf("@")<0 )
+    {
+        document.getElementById("mail_qq").innerHTML=mail_txt+"@qq.com";
+        document.getElementById("mail_126").innerHTML=mail_txt+"@126.com";
+        document.getElementById("mail_163").innerHTML=mail_txt+"@163.com";
+        document.getElementById("mail_sinaM").innerHTML=mail_txt+"@sina.com";
+        document.getElementById("mail_sinaN").innerHTML=mail_txt+"@sina.cn";
+        document.getElementById("mail_hotmail").innerHTML=mail_txt+"@hotmail.com";
+        document.getElementById("mail_gmail").innerHTML=mail_txt+"@gmail.com";
+        document.getElementById("mail_sohu").innerHTML=mail_txt+"@sohu.cn";
+        document.getElementById("mail_yahoo").innerHTML=mail_txt+"@yahoo.cn";
+        document.getElementById("mail_139").innerHTML=mail_txt+"@139.cn";                 
+     }
+}
+ function blurFn()
+ {
+	var title;
+	switch(this.id)
+	{
+	  case "mail":
+		   title="请填写有效的电子邮箱";
+		   break;
+	  case "pwd":
+		   title="密码应该由6-20个字母、数字或特殊字符组成";
+		   break;
+	  case "user_name":
+		   title="请填写你的真实中文姓名";
+		   break;
+	}
+	var t=new tip(title,"judge",this.id);
+	//先关闭原来的info tip
+	
+	//如果有错误的时候开启 error tip
+	t.hideTip();
+ }
+ function checkFormat(o)
+ {
+	// o 是要检查格式的dom对象
+	var x=judge(o.id);
+	return x;
+ }
+ function focusFn()
+ {
+	var title;
+	switch(this.id)
+	{
+	  case "mail":
+		   title="请填写有效的电子邮箱,推荐使用<br /><i>QQ邮箱</i>";
+		   break;
+	  case "pwd":
+		   title="由6-20个字母、数字或特殊字符组成";
+		   break;
+	  case "user_name":
+		   title="请填写你的真实姓名";
+		   break;
+	}
+	var t=new tip(title,"original",this.id);
+	t.showTip();
+ }
